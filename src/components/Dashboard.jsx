31948ee69a5e10f6ec796cf0c597143a0d2c1d4f
Nivar Sherwani <nivarsherwani0@gmail.com>
2025-07-13 12:49:18 -0400

    updated ui placement for mobile experience

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard({ data, logs }) {
     // console.count('Dashboard Render')
 
     return (
-        <div id="dashboard" className="flex flex-col gap-1 lg:gap-5 w-full lg:w-1/3 rounded-xl shadow-xl p-5 dashboard text-left"> 
+        <div id="dashboard" className="flex flex-col-reverse gap-1 gap-5 w-full lg:w-1/3 rounded-xl shadow-xl p-5 dashboard text-left lg:flex-col"> 
             <StaticUpdates logs={logs} />
             <LiveUpdates data={data} />
         </div>
